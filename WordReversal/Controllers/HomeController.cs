9b8786f091349af504a37178cbb112042d275100
robinsongz <mrrobinsung@gmail.com>
2019-05-22 17:55:45 -0700

    Add project files.

---
+++ WordReversal/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WordReversal.Models;
+
+namespace WordReversal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string input)
+        {
+            if (input == null)
+            {
+                return View();
+            }
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            ViewData["Input"] = new string(charArray);
+
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
